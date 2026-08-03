5535f3dc773fc8c88b1cf3776cb36ef760ba725e
Mahmudul <imdmahmudul237@gmail.com>
2026-08-03 18:07:41 +0600

    feat: add project documentation and initialize core store exports

--- stores/index.ts
+++ stores/index.ts
@@ -1,8 +1,8 @@
 import { chatSessionStore } from './ChatSessionStore';
 import { modelStore } from './ModelStore';
 
-export { chatSessionStore, type ChatMessage, type ChatSession, type LanguageLearningSettings } from './ChatSessionStore';
-export { modelStore, type CompletionParams, type LLMModel } from './ModelStore';
+export { chatSessionStore, type ChatMessage, type ChatSession, type AppSettings } from './ChatSessionStore';
+export { modelStore, type CompletionParams } from './ModelStore';
 
 // Initialize stores
 export const stores = {
